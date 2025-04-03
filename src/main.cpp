ce276e72638382922d13a50db9fa21b4c320862a
Bigfatgreat <kerksaphat99@gmail.com>
2025-04-03 09:08:37 +0700

    Commits SPIFFS to LittleFS cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,65 @@
 #include <Arduino.h>
+#include "LittleFS.h"
+#define FILESYSTEM LittleFS
 
-#include "FS.h"
-#include "SPIFFS.h"
+bool logging_enabled = true;
+unsigned long lastLogTime = 0;
+const unsigned long logInterval = 10000;
+String current_value = "N/A";
 
 void setup() {
-  Serial.begin(9600);
-  if (!SPIFFS.begin(true)) {
-    Serial.println("SPIFFS failed");
+  Serial.begin(115200);
+  if (!FILESYSTEM.begin(true)) {
+    Serial.println(F("LittleFS Mount Failed"));
     return;
   }
+  Serial.println(F("ESP32 Logger Ready"));
 }
 
-  void loop() {
-    
-  if (Serial.available()) {
-    String cmd = Serial.readStringUntil('\n');
-    if (cmd == "GET_LOG") {
-      File file = SPIFFS.open("/log.txt");
-      while (file.available()) {
-        Serial.write(file.read()); // or Serial.println(file.readStringUntil('\n'));
-      }
+void loop() {
+  if (logging_enabled && millis() - lastLogTime > logInterval) {
+    lastLogTime = millis();
+    float temp = random(200, 300) / 10.0;
+    float hum = random(400, 600) / 10.0;
+
+    File file = FILESYSTEM.open("/log.txt", FILE_APPEND);
+    if (file) {
+      file.printf("T:%.1f,H:%.1f,Time:%lu\n", temp, hum, millis() / 1000);
       file.close();
-    } else if (cmd == "LED_ON") {
-      digitalWrite(2, HIGH);
     }
   }
+
+    if (Serial.available()) {
+      String cmd = Serial.readStringUntil('\n');
+      cmd.trim();
+      if (cmd.startsWith("SET:")) {
+    current_value = cmd.substring(4);
+    Serial.println("Value updated");
+
+  } else if (cmd == "GET_VALUE") {
+    Serial.println(current_value);
   }
 
+    if (cmd == "GET_LOG") {
+      File file = FILESYSTEM.open("/log.txt");
+      if (file) {
+        while (file.available()) Serial.write(file.read());
+        file.close();
+      } else {
+        Serial.println(F("Can't open log.txt"));
+      }
+
+    } else if (cmd == "CLEAR_LOG") {
+      logging_enabled = false;
+      FILESYSTEM.remove("/log.txt");
+      delay(100);
+      File check = FILESYSTEM.open("/log.txt");
+      if (!check) Serial.println(F("Log cleared."));
+      else {
+        Serial.println(F("Log still exists!"));
+        check.close();
+      }
+      logging_enabled = true;
+    }
+  }
+}
